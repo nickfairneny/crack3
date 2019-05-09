3be762835cc5f6cb41c199f62bfec53e45e39502
Nick Fairneny <nickfairneny82@gmail.com>
2019-05-09 17:57:21 +0000

    First commit

--- crack.c
+++ crack.c
@@ -6,15 +6,17 @@
 
 const int PASS_LEN=50;        // Maximum any password can be
 const int HASH_LEN=33;        // Length of MD5 hash strings
+int size;
 
 
 // Stucture to hold both a plaintext password and a hash.
 struct entry 
 {
     // TODO: FILL THIS IN
+    char password[50];
+    char hash[33];
+    
 };
-
-
 
 // TODO
 // Read in the dictionary file and return an array of structs.
@@ -22,10 +24,54 @@ struct entry
 // word.
 struct entry *read_dictionary(char *filename, int *size)
 {
-    *size = 0;
-    return NULL;
+    int filelength = file_length(filename);
+    
+    FILE *c = fopen(filename, "r");
+    if(!c)
+    {
+        printf("Can't open %s for reading", filename);
+    }
+
+    char *contents = malloc(filelength);
+    fread(contents, 1, filelength, c);
+    fclose(c);
+    
+    int lines = 0;
+    
+    for (int i = 0; i < filelength; i++)
+    {
+        if (contents[i] == '\n') 
+        {
+            contents[i] = '\0';
+            lines++;
+        }
+    }
+    
+    struct entry *cc = malloc(lines * sizeof(struct entry));
+    
+    for (int i = 0; i < filelength; i++)
+    {
+        if (contents[i] == '\0')
+        {
+            strcopy(cc[0].password, &contents[0]);
+            cc[0].password = strlen(cc[0], password);
+            char *nextcc = &contents[i] + 1;
+            //cc[lines].password = nextcc;
+        }
+    }
+    
+    *size = lines;
+    return cc;
 }
 
+int comp(const void *a, const void *b)
+    {
+        
+        struct entry *ca = (struct entry *)a;    
+        struct entry *cb = (struct entry *)b; 
+        return strcmp((*ca).password, (*cb).password);
+        
+    }
 
 int main(int argc, char *argv[])
 {
@@ -34,16 +80,22 @@ int main(int argc, char *argv[])
         printf("Usage: %s hash_file dict_file\n", argv[0]);
         exit(1);
     }
-
+    
+    int dictlen = file_length(argv[1]);
     // TODO: Read the dictionary file into an array of entry structures
-    struct entry *dict = read_dictionary(NULL, NULL);
+    struct entry *dict = read_dictionary(argv[1], dictlen);
     
     // TODO: Sort the hashed dictionary using qsort.
     // You will need to provide a comparison function.
-    qsort(dict, 0, 0, NULL);
+    qsort(dict, size, sizeof(struct entry), comp(dict[0], dict[1]));
 
     // TODO
     // Open the hash file for reading.
+    FILE *h = fopen(argv[2], "r");
+    if(!h)
+    {
+        printf("Can't open %s for reading", argv[2]);
+    }
 
     // TODO
     // For each hash, search for it in the dictionary using
@@ -51,4 +103,7 @@ int main(int argc, char *argv[])
     // If you find it, get the corresponding plaintext dictionary word.
     // Print out both the hash and word.
     // Need only one loop. (Yay!)
+    
+    
+    
 }
